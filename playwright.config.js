caa0c7b43e61f4957a4cbad208a8c2da9bf5c818
vineelan09 <vineelan09@gmail.com>
2025-08-19 18:00:45 -0400

    Add CI workflow

--- playwright.config.js
+++ playwright.config.js
@@ -30,6 +30,9 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
     trace: 'on-first-retry',
   },
 
